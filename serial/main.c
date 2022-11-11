7241917372c38d96afc0c67a673b1258f15b3eef
felix <74864332+felix-blaz@users.noreply.github.com>
2022-11-11 19:53:19 +0000

    Add files via upload

--- serial/main.c
+++ serial/main.c
@@ -213,6 +213,66 @@ void neighbourSearch(int** currentWorld, int** futureWorld)
 }
 
 
+void cellCount(int** currentWorld, int* countS, int* countI, int* countE,
+	int* countR, int* countD )
+{
+	//these values will be incremented when ever it cell is present
+	*countS = 0;
+	*countI = 0;
+	*countD = 0;
+	*countR = 0;
+	*countE = 0;
+
+	for (int i = 0; i < Total_Rows; i++)
+	{
+		for (int j = 0; j < Total_Cols; j++)
+		{
+
+			if (currentWorld[i][j] == S)
+			{
+				(*countS)++;
+			}
+			else if (currentWorld[i][j] == I)
+			{
+				(*countI)++;
+			}
+			else if (currentWorld[i][j] == E)
+			{
+				(*countE)++;
+			}
+			else if (currentWorld[i][j] == D)
+			{
+				(*countD)++;
+			}
+			else
+			{
+				(*countR)++;
+			}
+			//copying the current world to the future world
+
+		}
+	}
+	
+}
+
+
+
+   // create a two-dimensional matrix dynamically
+int** createTwoDimenArray(const int rows, const int cols)
+{
+    // create two-d pointers
+    int** arr2D = (int**)malloc(rows * sizeof(int*));
+
+   	 // create the memory
+    int* memory = (int*)malloc(rows * cols * sizeof(int));
+
+   	 // adjust pointers with memory
+    for (int i = 0; i < rows; i++)
+    {
+        arr2D[i] = &memory[i * cols];
+    }
+    return arr2D;
+}
 
 
 
